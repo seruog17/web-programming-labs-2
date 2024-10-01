cfb8ce8230420464ef6f0e10908e3b2b4eb8816d
Посаженников Сергей <posagennikov17@gmail.com>
2024-10-02 00:59:02 +0700

    Стили

--- app.py
+++ app.py
@@ -38,9 +38,13 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for("static", filename="oak.jpg")
-    return '''
+    css_url = url_for("static", filename="lab1.css")
+    return f'''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="{css_url}">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img src="''' + path + '''">
