2e7749e93c7cfe3d67d687992e47c89ff6317b71
Посаженников Сергей <posagennikov17@gmail.com>
2024-10-15 23:58:40 +0700

    Имя студента

--- app.py
+++ app.py
@@ -304,7 +304,8 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Сергей Посаженников'
+    return render_template('example.html', name=name)
 
 
 
