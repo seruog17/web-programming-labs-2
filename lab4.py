e46365b868228fec1f29a02f4686e07a3faaf525
Посаженников Сергей <posagennikov17@gmail.com>
2024-11-17 18:49:26 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -21,8 +21,71 @@ def div():
     if x2 == '0':
         return render_template('lab4/div.html', error='На ноль делить нельзя!')
     
-    
+
     x1 = int(x1)
     x2 = int(x2)
     result = x1 / x2
     return render_template('lab4/div.html' , x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum_form.html')
+
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1 = request.form.get('x1', 0, type=int)
+    x2 = request.form.get('x2', 0, type=int)
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul_form.html')
+
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1', 1, type=int)
+    x2 = request.form.get('x2', 1, type=int)
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub_form.html')
+
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow_form.html')
+
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+    if x1 == '0' and x2 == '0':
+        return render_template('lab4/pow.html', error='Ноль в нулевой степени не определен!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
