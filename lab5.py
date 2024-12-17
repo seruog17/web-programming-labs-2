c22a985df98a3ac1fedf9d040afb0ab6e7602b2e
Посаженников Сергей <posagennikov17@gmail.com>
2024-12-18 03:22:58 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -91,6 +91,25 @@ def list_articles():
     return render_template('lab5/lab5.html')
 
 
-@lab5.route('/lab5/create')
-def create_article():
-    return render_template('lab5/lab5.html')
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return render_template('lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s", (login, ))
+    login_id = cur.fetchone()["id"]
+
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+                VALUES ({login_id}, '{title}', '{article_text}');")
+    
+    db_close(conn, cur)
+    return redirect('/lab5')
