378a52b5d0033aab90cffd2d77ddf1838650d02d
Посаженников Сергей <posagennikov17@gmail.com>
2024-12-23 20:48:15 +0700

    Заготовка к 6 лабораторной

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route("/index")
 def index():
@@ -33,6 +35,7 @@ def index():
                 <li><a href="/lab3">Третья лабораторная</a></li>
                 <li><a href="/lab4">Четвёртая лабораторная</a></li>
                 <li><a href="/lab5">Пятая лабораторная</a></li>
+                <li><a href="/lab6">Шестая лабораторная</a></li>
             </ol>
         </div>
         <footer>
